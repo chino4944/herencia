bed9833c7691b7c44e5afef9f6aad5c1fe5f4dc6
Fernando TUF W <chinos4944@gmail.com>
2023-10-10 22:13:57 -0400

    medio adelantado

---
+++ HerenciaEj2/src/herenciaej2/entidades/Electrodomestico.java
@@ -0,0 +1,60 @@
+package herenciaej2.entidades;
+
+public class Electrodomestico {
+
+    protected Double precio;
+    protected String color;
+    protected Character consumoEnergetico;
+    protected Double peso;
+
+    public Electrodomestico() {
+    }
+
+    public Electrodomestico(Double precio, String color, Character consumoEnergetico, Double peso) {
+        this.precio = precio;
+        this.color = color;
+        this.consumoEnergetico = consumoEnergetico;
+        this.peso = peso;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Character getConsumoEnergetico() {
+        return consumoEnergetico;
+    }
+
+    public void setConsumoEnergetico(Character consumoEnergetico) {
+        this.consumoEnergetico = consumoEnergetico;
+    }
+
+    public Double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Double peso) {
+        this.peso = peso;
+    }
+
+    public void comprobarConsumoElectrico(Character letra){
+        
+    }
+    
+    public void comprobarColor(String color){
+        
+    }
+    
+}
